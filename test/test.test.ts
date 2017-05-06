a650bc2dcc94c644b8bc86ded2382e24b1cf97b5
Maxfield Walker <maxfield.walker@gmail.com>
2017-05-06 11:27:24 +0900

    add color palette creation test

--- test/test.test.ts
+++ test/test.test.ts
@@ -6,18 +6,20 @@ import { ColorPalette } from "../src/types";
 import { replace } from "../src/replace";
 
 
-suite("test", () => {
-    test("test", () => {
-        const result = createColorPalette("./test/baselines/test.json");
-
-        console.log(result);
-    });
-
-    test("replacement test", () => {
+suite("tests", () => {
+    test("replacement", () => {
         const json = parseJson("./test/baselines/test2.json");
         const colorPalette = createColorPalette("./test/baselines/test2.json");
         const replaced = replace(json, colorPalette);
         console.log(JSON.stringify(replaced, undefined, 4));
 
+    });
+
+    test("create color palette", () => {
+        const json = parseJson("./test/baselines/colorPalette.json");
+        const colorPalette = createColorPalette(json);
+
+        assert.deepEqual(colorPalette.getColor("$color"), "#CFCFCF");
+        assert.deepEqual(colorPalette.getAlpha("@alpha"), "80");
     });
 });
